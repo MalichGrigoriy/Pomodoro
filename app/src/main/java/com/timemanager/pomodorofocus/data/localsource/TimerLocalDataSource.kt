ec844020a1e0c8a56d21ab3d2640df55d68f2ba9
Malych Grygorii <malich.grigoriy@gmail.com>
2026-02-08 14:38:49 +0200

    storageKey for TimerType save and restore after obfuscation

--- app/src/main/java/com/timemanager/pomodorofocus/data/localsource/TimerLocalDataSource.kt
+++ app/src/main/java/com/timemanager/pomodorofocus/data/localsource/TimerLocalDataSource.kt
@@ -4,8 +4,8 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
+import com.timemanager.pomodorofocus.domain.settings.TIMER_TYPE_DEFAULT
 import com.timemanager.pomodorofocus.ui.timer.simpleView.TimerType
-import com.timemanager.pomodorofocus.ui.timer.simpleView.timerTypeByName
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
@@ -16,12 +16,12 @@ class TimerLocalDataSource @Inject constructor(private val dataStore: DataStore<
 
     fun getTimerType(): Flow<TimerType> {
         return dataStore.data.map { preferences ->
-            val type: String = preferences[TIMER_KEY] ?: TimerType.String.name
-            timerTypeByName(type)
+            val typeKey: String = preferences[TIMER_KEY] ?: TIMER_TYPE_DEFAULT.storageKey
+            TimerType.fromStorageKey(typeKey)
         }
     }
 
     suspend fun setTimerType(timerType: TimerType) {
-        dataStore.edit { it[TIMER_KEY] = timerType.name }
+        dataStore.edit { it[TIMER_KEY] = timerType.storageKey }
     }
 }
--- app/src/main/java/com/timemanager/pomodorofocus/ui/timer/simpleView/TimerType.kt
+++ app/src/main/java/com/timemanager/pomodorofocus/ui/timer/simpleView/TimerType.kt
@@ -3,22 +3,17 @@ package com.timemanager.pomodorofocus.ui.timer.simpleView
 private const val SECOND_MS = 1000L
 private const val HUNDREDTH_SECOND_MS = 10L
 
-enum class TimerType(val delay: Long) {
-    String(SECOND_MS),
-    StringLong(HUNDREDTH_SECOND_MS),
-    Dual(SECOND_MS),
-    Triple(HUNDREDTH_SECOND_MS),
-    LedMatrix(SECOND_MS),
-    Empty(SECOND_MS)
-}
+enum class TimerType(val delay: Long, val storageKey: String) {
+    String(SECOND_MS, "String"),
+    StringLong(HUNDREDTH_SECOND_MS, "StringLong"),
+    Dual(SECOND_MS, "Dual"),
+    Triple(HUNDREDTH_SECOND_MS, "Triple"),
+    LedMatrix(SECOND_MS, "LedMatrix"),
+    Empty(SECOND_MS, "Empty");
 
-fun timerTypeByName(type: String?): TimerType {
-    return when (type) {
-        TimerType.Dual.name -> TimerType.Dual
-        TimerType.Triple.name -> TimerType.Triple
-        TimerType.String.name -> TimerType.String
-        TimerType.StringLong.name -> TimerType.StringLong
-        TimerType.LedMatrix.name -> TimerType.LedMatrix
-        else -> TimerType.String
+    companion object {
+        fun fromStorageKey(key: String?): TimerType {
+            return entries.find { it.storageKey == key } ?: String
+        }
     }
 }
